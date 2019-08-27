ce984691bc7404cdbf1ba1ad020835718090d6b5
Eugene <eugenax2017@gmail.com>
2019-08-27 14:45:37 +0200

    First on test

--- Main30.java
+++ Main30.java
@@ -1,6 +1,11 @@
+//если нет конструктора по умолчанию и программа ругается на это
+//в этом случае нужно в конструкторы потомка добавить super() с параметрами для конструктора предка
+
+import java.io.IOException;
+
 public class Main30 {
-    public static void main(String[] args) {
-        Cosa2 Cosa = new Cosa2(3, "R");
+    public static void main(String[] args) throws IOException {
+        Cosa2 Cosa = new Cosa2(3);
         System.out.println(Cosa.value);
         Cosa1 Cosa8 = (Cosa1) Cosa;
         System.out.println(Cosa8.getClass());
@@ -11,8 +16,8 @@ public class Main30 {
 class Cosa1 {
     int index;
     String value;
-    public Cosa1() {this.value = "Super ";}
-    public Cosa1(int index, String value){
+    //public Cosa1() {this.value = "Super ";}
+    public Cosa1(int index){
         this.index = index;
         this.value += "Cosa1 ";
     }
@@ -22,8 +27,10 @@ class Cosa1 {
 }
 
 class Cosa2 extends Cosa1 {
-    public Cosa2(int index, String value){
-        this.index = index;
+    public Cosa2(){ super(0);}
+    public Cosa2(int index) {
+        super(index);
+        //this.index = index;
         this.value += "Cosa2 ";
     }
     public int defineClass(){
--- lesson1/Unit1.java
+++ lesson1/Unit1.java
@@ -1,4 +1,21 @@
-package Lesson1;
+package lesson1;
 
 public class Unit1 {
+    public static void main(String[] args) {
+        B Value = new C();
+        System.out.println(Value.returnMe());
+    }
 }
+class B {
+    int x;
+    B( int n ) { x = n; } // a constructor
+    int returnMe() { return x; }
+}
+
+class C extends B {
+    C () { super(0); } // a constructor
+    C (int n) { super(n); } // a constructor
+}
+
+//пример, показывающий //если нет конструктора по умолчанию и программа ругается на это
+////в этом случае нужно в конструкторы потомка добавить super() с параметрами для конструктора предка
